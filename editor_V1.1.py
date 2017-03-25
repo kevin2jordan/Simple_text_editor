a1d3bc57d2315512d5089f423fb2c26c2da5d39a
rahul kumar <r970ahul@gmail.com>
2017-03-25 15:46:20 +0530

    Add files via upload

---
+++ editor_V1.1.py
@@ -0,0 +1,196 @@
+import sys
+from PyQt4 import QtGui, QtCore
+from PyQt4.QtCore import Qt
+
+class Main(QtGui.QMainWindow):
+
+    def __init__(self, parent = None):
+        QtGui.QMainWindow.__init__(self,parent)
+        self.filename = ""
+        self.initUI()
+
+    def initToolbar(self):
+
+        self.newAction = QtGui.QAction(QtGui.QIcon("icons/new.png"),"New",self)
+        self.newAction.setShortcut("Ctrl+N")
+        self.newAction.setStatusTip("Create a new document from scratch.")
+        self.newAction.triggered.connect(self.new)
+
+        self.openAction = QtGui.QAction(QtGui.QIcon("icons/open.png"),"Open file",self)
+        self.openAction.setStatusTip("Open existing document")
+        self.openAction.setShortcut("Ctrl+O")
+        self.openAction.triggered.connect(self.open)
+
+        self.saveAction = QtGui.QAction(QtGui.QIcon("icons/save.png"),"Save",self)
+        self.saveAction.setStatusTip("Save document")
+        self.saveAction.setShortcut("Ctrl+S")
+        self.saveAction.triggered.connect(self.save)
+
+        self.printAction = QtGui.QAction(QtGui.QIcon("icons/print.png"),"Print document",self)
+        self.printAction.setStatusTip("Print document")
+        self.printAction.setShortcut("Ctrl+P")
+        self.printAction.triggered.connect(self.printHandler)
+
+        self.previewAction = QtGui.QAction(QtGui.QIcon("icons/preview.png"),"Page view",self)
+        self.previewAction.setStatusTip("Preview page before printing")
+        self.previewAction.setShortcut("Ctrl+Shift+P")
+        self.previewAction.triggered.connect(self.preview)
+
+        self.cutAction = QtGui.QAction(QtGui.QIcon("icons/cut.png"),"Cut to clipboard",self)
+        self.cutAction.setStatusTip("Delete and copy text to clipboard")
+        self.cutAction.setShortcut("Ctrl+X")
+        self.cutAction.triggered.connect(self.text.cut)
+
+        self.copyAction = QtGui.QAction(QtGui.QIcon("icons/copy.png"),"Copy to clipboard",self)
+        self.copyAction.setStatusTip("Copy text to clipboard")
+        self.copyAction.setShortcut("Ctrl+C")
+        self.copyAction.triggered.connect(self.text.copy)
+
+        self.pasteAction = QtGui.QAction(QtGui.QIcon("icons/paste.png"),"Paste from clipboard",self)
+        self.pasteAction.setStatusTip("Paste text from clipboard")
+        self.pasteAction.setShortcut("Ctrl+V")
+        self.pasteAction.triggered.connect(self.text.paste)
+
+        self.undoAction = QtGui.QAction(QtGui.QIcon("icons/undo.png"),"Undo last action",self)
+        self.undoAction.setStatusTip("Undo last action")
+        self.undoAction.setShortcut("Ctrl+Z")
+        self.undoAction.triggered.connect(self.text.undo)
+
+        self.redoAction = QtGui.QAction(QtGui.QIcon("icons/redo.png"),"Redo last undone thing",self)
+        self.redoAction.setStatusTip("Redo last undone thing")
+        self.redoAction.setShortcut("Ctrl+Y")
+        self.redoAction.triggered.connect(self.text.redo)
+
+        bulletAction = QtGui.QAction(QtGui.QIcon("icons/bullet.png"),"Insert bullet List",self)
+        bulletAction.setStatusTip("Insert bullet list")
+        bulletAction.setShortcut("Ctrl+Shift+B")
+        bulletAction.triggered.connect(self.bulletList)
+
+        numberedAction = QtGui.QAction(QtGui.QIcon("icons/number.png"),"Insert numbered List",self)
+        numberedAction.setStatusTip("Insert numbered list")
+        numberedAction.setShortcut("Ctrl+Shift+L")
+        numberedAction.triggered.connect(self.numberList)
+
+        self.toolbar = self.addToolBar("Options")
+
+        self.toolbar.addAction(self.newAction)
+        self.toolbar.addAction(self.openAction)
+        self.toolbar.addAction(self.saveAction)
+
+        self.toolbar.addSeparator()
+
+        self.toolbar.addAction(self.printAction)
+        self.toolbar.addAction(self.previewAction)
+
+        self.toolbar.addSeparator()
+
+        self.toolbar.addAction(self.cutAction)
+        self.toolbar.addAction(self.copyAction)
+        self.toolbar.addAction(self.pasteAction)
+        self.toolbar.addAction(self.undoAction)
+        self.toolbar.addAction(self.redoAction)
+
+        self.toolbar.addSeparator()
+
+        self.toolbar.addAction(bulletAction)
+        self.toolbar.addAction(numberedAction)
+
+        self.addToolBarBreak()
+
+    def initFormatbar(self):
+
+      self.formatbar = self.addToolBar("Format")
+
+
+    def initMenubar(self):
+
+      menubar = self.menuBar()
+
+      file = menubar.addMenu("File")
+      edit = menubar.addMenu("Edit")
+      help = menubar.addMenu("View")
+
+      file.addAction(self.newAction)
+      file.addAction(self.openAction)
+      file.addAction(self.saveAction)
+      file.addAction(self.printAction)
+      file.addAction(self.previewAction)
+
+      edit.addAction(self.undoAction)
+      edit.addAction(self.redoAction)
+      edit.addAction(self.cutAction)
+      edit.addAction(self.copyAction)
+      edit.addAction(self.pasteAction)
+
+    def initUI(self):
+
+        self.text = QtGui.QTextEdit(self)
+        self.initToolbar()
+        self.initFormatbar()
+        self.initMenubar()
+        self.text.setTabStopWidth(33)
+        self.setCentralWidget(self.text)
+        self.statusbar = self.statusBar()
+        self.text.cursorPositionChanged.connect(self.cursorPosition)
+        self.setGeometry(100,100,1030,800)
+        self.setWindowTitle("Personal Editor")
+        self.setWindowIcon(QtGui.QIcon("icons/pyt.png"))
+
+    def new(self):
+
+        spawn = Main(self)
+        spawn.show()
+
+    def open(self):
+        self.filename = QtGui.QFileDialog.getOpenFileName(self, 'Open File')
+
+        if self.filename:
+            with open(self.filename,"rt") as file:
+                self.text.setText(file.read())
+
+    def save(self):
+        if not self.filename:
+          self.filename = QtGui.QFileDialog.getSaveFileName(self, 'Save File')
+        with open(self.filename,"wt") as file:
+            file.write(self.text.toPlainText())
+
+
+    def preview(self):
+        preview = QtGui.QPrintPreviewDialog()
+        preview.paintRequested.connect(lambda p: self.text.print_(p))
+
+        preview.exec_()
+
+    def printHandler(self):
+        dialog = QtGui.QPrintDialog()
+
+        if dialog.exec_() == QtGui.QDialog.Accepted:
+            self.text.document().print_(dialog.printer())
+
+    def cursorPosition(self):
+        cursor = self.text.textCursor()
+        line = cursor.blockNumber() + 1
+        col = cursor.columnNumber()
+        self.statusbar.showMessage("Line: {} | Column: {}".format(line,col))
+
+    def bulletList(self):
+
+        cursor = self.text.textCursor()
+        cursor.insertList(QtGui.QTextListFormat.ListDisc)
+
+    def numberList(self):
+
+        cursor = self.text.textCursor()
+        cursor.insertList(QtGui.QTextListFormat.ListDecimal)
+
+def main():
+
+    app = QtGui.QApplication(sys.argv)
+
+    main = Main()
+    main.show()
+
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
